ddbf9e885f8782577b630e443095503756f96cbe
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-02 16:52:05 -0300

    feat: create handleError and validate serializer

---
+++ src/middlewares/validateSerializer.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express'
+import { ValidationError } from "yup";
+
+const validateSerializerMiddleware = (serialize: any) => async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    await serialize.validate(req.body, {
+      stripUnknown: true,
+      abortEarly: false,
+    })
+
+    return next()
+  } catch (err) {
+    const error = err as ValidationError
+    return res.status(400).json({ message: error.errors })
+  }
+}
+
+export default validateSerializerMiddleware
