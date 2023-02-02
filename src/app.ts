da8166c969c31b995e3afd18aad90732dba38e8a
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-02 16:53:10 -0300

    fix: run migrations

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
+import clientRouter from './routes/client.routes'
+import handleErrorMiddleware from './middlewares/handleError.middleware';
 
 const app = express()
 app.use(express.json())
 
+app.use('/clients', clientRouter)
+
+app.use(handleErrorMiddleware)
 export default app
