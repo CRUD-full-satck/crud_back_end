dc32ea47e4a4dcd2d684a8af380c247adf148521
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-02 18:12:47 -0300

    feat: verify atuth token

---
+++ src/middlewares/verifyAuthToken.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import "dotenv/config"
+
+const verifyAuthTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    let token = req.headers.authorization
+    if (!token) {
+        return res.status(401).json({
+            message: "Missing token"
+        })
+    }
+
+    token = token.split(" ")[1]
+
+    jwt.verify(token, process.env.SECRET_KEY as string, (err, decoded: any) => {
+        if(err) {
+            return res.status(401).json({
+                message: "Invalid token"
+            })
+        }
+
+        req.client = {
+            id: decoded.sub
+        }
+        return next()
+    })
+}
+
+export default verifyAuthTokenMiddleware
