763f60c139ee703c38d5a15aaf64b3bad1e915cb
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-04 10:49:43 -0300

    fixing deployment error

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
-import "dotenv/config";
 import AppDataSource from "./data-source";
+
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error("Error during Data Source initialization", err);
