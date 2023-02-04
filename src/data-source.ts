aa252db649cdb9b90b69913c2925fadc7fa03211
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-03 23:49:04 -0300

    fixing deployment error

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,17 @@ const dataSourceConfig = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
   const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
 
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+
   return {
     type: "postgres",
     host: process.env.HOST,
